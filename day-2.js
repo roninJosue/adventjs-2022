d203bc8a29bafa6d76ed3d2f4febb2961302efd3
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-12-03 21:20:08 -0600

    Add Problem's link

--- day-2.js
+++ day-2.js
@@ -1,3 +1,5 @@
+// Link https://adventjs.dev/challenges/2022/2
+
 const year = 2023
 const holiday = ['01/06', '04/01', '12/25']
 
@@ -6,7 +8,8 @@ console.log(countHours(year, holiday))
 function countHours(year, holidays) {
   const calculateHours = (totalHours, date) => {
     const day = new Date(`${year}/${date}`).getDay()
-    return day !== 0 && day !== 6 ? totalHours + 2 : totalHours
+    const isDayOfWeek = day !== 0 && day !== 6
+    return isDayOfWeek ? totalHours + 2 : totalHours
   }
   return holidays.reduce(calculateHours, 0)
 }
