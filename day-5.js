92410647f6e36b10e7013263923e95f500dbc285
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-12-07 21:54:15 -0600

    Change algorithm of day-5.js problem

--- day-5.js
+++ day-5.js
@@ -1,21 +1,28 @@
-const giftsCities = [12, 3, 11, 5, 7]
-const maxGifts = 20
-const maxCities = 3
+const giftsCities = [40, 70, 40]
+const maxGifts = 100
+const maxCities = 2
 
-getMaxGifts(giftsCities, maxGifts, maxCities)
+console.log(getMaxGifts(giftsCities, maxGifts, maxCities))
 
 function getMaxGifts(giftsCities, maxGifts, maxCities) {
   let max = giftsCities[0]
-  let prevSum = 0
+  //let prevSum = 0
+  let countCities = 0
 
   if (giftsCities[0] > maxGifts) return 0
   if (giftsCities.length === 1 && giftsCities[0] <= maxGifts) return giftsCities[0]
 
-  for (let i = 1; i < giftsCities.length; i++) {
-    let temp = Math.max(max, prevSum)
-    max = prevSum + giftsCities[i]
-    prevSum = temp
+  for (let i = 1; i < giftsCities.length && countCities < maxCities; i++) {
+    if (max === maxGifts) return max
+    if (max + giftsCities[i] > maxGifts) {
+      //countCities++
+      max = Math.max(max, giftsCities[i]) > maxGifts ? max : Math.max(max, giftsCities[i])
+    } else {
+      countCities++
+      max = max + giftsCities[i]
+    }
   }
 
-  console.log(max, prevSum)
+  console.log(`max:${max}`)
+  return max
 }
